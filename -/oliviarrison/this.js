bc6717da3b71d002909f2522ef20d1b34250b0d3
nsq <101462354+nsqx@users.noreply.github.com>
2022-06-24 15:31:53 +0800

    Update this.js

--- -/oliviarrison/this.js
+++ -/oliviarrison/this.js
@@ -1,9 +1,23 @@
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", (function() {
     _pseudo = document.querySelector("pseudo")
+    _colr = document.querySelector("colr")
+    _slate = document.querySelector("slate")
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    var trident = ua.indexOf('Trident/');
+    if (msie > 0) {
+    _slate.style.display = "block"
+    }else if (trident > 0) {
+        _slate.style.display = "block"
+    }
+
     try {
     //_pseudo.scrollTop = localStorage.getItem("current-scroll")
-    _pseudo.scrollTo({top: localStorage.getItem("current-scroll"), behavior:"smooth"})
-    } catch {}
+    _pseudo.scrollTo({top: localStorage.getItem("current-scroll")})
+    } catch (err) {
+        console.log("ok then")
+    }
 
     __0 = document.querySelector("#hhr0")
     __1 = document.querySelector("#hhr1")
@@ -11,23 +25,29 @@ document.addEventListener("DOMContentLoaded", ()=> {
     __3 = document.querySelector("#hhr3")
     __4 = document.querySelector("#hhr4")
     __5 = document.querySelector("#hhr5")
+    __scrstyle = document.querySelector("style#rstylebsxsettings")
 
     if (window.location.search == "?~0"){
         __0.scrollIntoView({behavior: "smooth", block:"start"})
     }else if (window.location.search == "?~1"){
         __1.scrollIntoView({behavior: "smooth", block:"start"})
     }else if (window.location.search == "?~2"){
+        __2.scrollIntoView({behavior: "smooth", block:"end"})
         __2.scrollIntoView({behavior: "smooth", block:"start"})
     }else if (window.location.search == "?~3"){
+        __3.scrollIntoView({behavior: "smooth", block:"end"})
         __3.scrollIntoView({behavior: "smooth", block:"start"})
     }else if (window.location.search == "?~4"){
+        __4.scrollIntoView({behavior: "smooth", block:"end"})
         __4.scrollIntoView({behavior: "smooth", block:"start"})
     }else if (window.location.search == "?~5"){
+        __5.scrollIntoView({behavior: "smooth", block:"end"})
         __5.scrollIntoView({behavior: "smooth", block:"start"})
-    }else
+    }
 
     lscrol = 0
-    _pseudo.addEventListener("scroll", ()=>{
+    _pseudo.addEventListener("scroll", (function(){
         localStorage.setItem("current-scroll", _pseudo.scrollTop)
-    })
-})
+        __scrstyle.innerHTML = "::-webkit-scrollbar-thumb {min-height:" + _pseudo.scrollTop / _pseudo.scrollHeight * window.innerHeight/2 + "px !important}"
+    }))
+}))
